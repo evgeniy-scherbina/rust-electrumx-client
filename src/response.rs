c598a722b98d3698ae8d5ebc2b420b31bf426018
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2018-12-10 15:32:44 +0200

    make all response fields public

--- src/response.rs
+++ src/response.rs
@@ -1,26 +1,26 @@
 #[derive(Debug, Deserialize)]
 pub struct GetBlockHeadersResponse {
-    count: usize,
+    pub count: usize,
     pub hex:   String,
-    max:   usize,
+    pub max:   usize,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct GetBalanceResponse {
-    confirmed:   u64,
-    unconfirmed: u64,
+    pub confirmed:   u64,
+    pub unconfirmed: u64,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct GetHistoryResponse {
-    height:  usize,
+    pub height:  usize,
     pub tx_hash: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct GetListUnspentResponse {
-    height:  usize,
-    tx_hash: String,
-    tx_pos:  usize,
-    value:   usize,
+    pub height:  usize,
+    pub tx_hash: String,
+    pub tx_pos:  usize,
+    pub value:   usize,
 }
